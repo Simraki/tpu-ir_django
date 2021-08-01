776ff0feb87f6d0fb64bd1441a723c88055a8bd6
Simraki <Simraki@mail.ru>
2021-08-01 14:50:34 +0700

    add filtering

--- api/views.py
+++ api/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
+from url_filter.integrations.drf import DjangoFilterBackend
 
 from api.models import *
 from . import serializers
 
 
 class CountryView(viewsets.ReadOnlyModelViewSet):
-    queryset = Country.objects.all()
+    queryset = Country.objects.order_by('name')
     serializer_class = serializers.CountrySerializer
 
 
 class CompanyView(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = serializers.CompanySerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['id_country']
