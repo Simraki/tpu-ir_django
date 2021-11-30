f54a5291ab69af0de53575b1172a82bada594460
Simraki <Simraki@mail.ru>
2021-11-30 18:19:26 +0700

    add agreement type to agreement serializer

--- api/serializers.py
+++ api/serializers.py
@@ -29,17 +29,18 @@ class RepresentativeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class AgreementTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AgreementType
+        fields = '__all__'
+
+
 class AgreementSerializer(serializers.ModelSerializer):
     partner = PartnerSerializer(read_only=True, source='id_partner')
     company = CompanySerializer(read_only=True, source='id_partner__id_company')
     representative = RepresentativeSerializer(read_only=True, source='id_representative')
+    agreement_type = AgreementTypeSerializer(read_only=True, source='id_agr_type')
 
     class Meta:
         model = Agreement
-        fields = '__all__'
-
-
-class AgreementTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AgreementType
         fields = '__all__'
